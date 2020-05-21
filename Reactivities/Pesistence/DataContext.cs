fdf3e93d8f86bcfedab64496e9463da1f47c3d39
Javier Encina <encinajavier7@gmail.com>
2020-05-21 09:02:08 -0300

    Add EntityFramework

---
+++ Reactivities/Pesistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Pesistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) :  base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
